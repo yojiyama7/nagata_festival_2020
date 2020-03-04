59b50785f74a242fd1d25efa1b0f10313f40eaf8
yojiyama7 <yojiyama7@gmail.com>
2020-03-04 17:08:06 +0900

    init and MenuButton

---
+++ src/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <TheMenuButton />
+  </div>
+</template>
+
+<script>
+import TheMenuButton from './TheMenuButton'
+
+export default {
+  components: {
+    TheMenuButton
+  }
+}
+
+</script>
+
+<style lang='scss' scoped>
+</style>
---
+++ src/components/TheMenuButton.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="box" :class="classObject" @click="toggleMenu()">
+    <div class="box-inner" :class="classObject">
+      <div class="bar bar-1" :class="classObject"></div>
+      <div class="bar bar-2" :class="classObject"></div>
+      <div class="bar bar-3" :class="classObject"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // isMenuOpen を多分 props に移す
+  // props: {
+  // },
+  data: function () {
+    return {
+      isMenuOpen: false
+    }
+  },
+  computed: {
+    classObject: function () {
+      return {
+        'menu-open': this.isMenuOpen
+      }
+    }
+  },
+  methods: {
+    toggleMenu: function () {
+      if (this.isMenuOpen) {
+        this.isMenuOpen = false
+      } else {
+        this.isMenuOpen = true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// グローバルなscssに書いておきたいものたち
+$white: #FAFAFA;
+$vermilion: #E53D46;
+$indigo: #42378A;
+$lime: #8CC63F;
+
+$trans-speed: 250ms;
+
+$box-size: 72px;
+.box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  height: $box-size;
+  width: $box-size;
+  border-radius: $box-size/2;
+  background-color: $indigo;
+  &-inner {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: $box-size * 0.5;
+    width: $box-size * 0.56;
+    // background-color: red;
+    transition-duration: $trans-speed;
+  }
+}
+
+$bar-height: 6px;
+.bar {
+  height: $bar-height;
+  width: 100%;
+  border-radius: $bar-height/2;
+  // width: $box-inner-size;
+  background-color: $white;
+  transform-origin: $bar-height/2 center 0;
+  transition-duration: $trans-speed;
+}
+
+.menu-open {
+  &.box-inner {
+    width: $box-size * 0.5;
+  }
+  &.bar-1 {
+    transform: rotate(45deg);
+    width: ($box-size*0.5 - $bar-height)*1.414 + $bar-height;
+  }
+  &.bar-2 {
+    transition-duration: $trans-speed*0.8;
+    width: 0px;
+  }
+  &.bar-3 {
+    transform: rotate(-45deg);
+    width: ($box-size*0.5 - $bar-height)*1.41421356 + $bar-height;
+  }
+}
+
+</style>
